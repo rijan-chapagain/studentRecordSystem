71b245eccd99490cc571a15884d7fd50a2afbe2c
rijan <hrijon.chp@gmail.com>
2020-04-10 10:37:26 +0800

    handeling request to enter student detail

--- js/requestHandlers.js
+++ js/requestHandlers.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 
 /**
  * reqStart sends status report to server and display html form on client 
+ * generate form with 3 options to choose where to go
+ * it will send client to requested page
  * 
  * @param {object} request 
  * @param {object} response 
@@ -12,6 +14,11 @@ var fs = require('fs');
  */
 function reqStart(request, response, postData){
     console.log("Request handler 'start' was called.");
+
+    fs.readFile('../index.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }       
    
     var body = '<html>'+
         '<head>'+
@@ -19,8 +26,8 @@ function reqStart(request, response, postData){
         'charset=UTF-8" />'+
         '</head>'+
         '<body>'+
-        '<form action="#" method="POST">' +
-        'Student Form: <input type="radio" name="degree" ><br>'+
+        '<form action="/student" enctype="multipart/form-data" method="POST">' +
+        'Input Student Details: <input type="radio" name="degree" ><br>'+
         'View Details: <input type="radio" name="degree" ><br>'+
         'Upload Image: <input type="radio" name="degree" ><br>'+
         '<input type="submit" name="submit" value="Submit">'+
@@ -28,14 +35,49 @@ function reqStart(request, response, postData){
         '</form>'+
         '</body>'+
         '</html>';
+    // var start = document.getElementById('start')
+    // var student = document.getElementById('student')
 
     response.writeHead( 200, {"Content-Type": "text/plain"} );
     response.write(body);
     response.end();
+});
 }
 
-function afterParse(params) {
-    
+
+/**
+ * sends status report to server and display html for on client side
+ * generate a form to enter student details
+ * 
+ * @param {object} request 
+ * @param {object} response 
+ * @param {object} postData 
+ */
+function reqStudent(request, response, postData){
+    console.log("Request handler 'student' was called.");
+
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+        '<form action="/start" enctype="multipart/form-data" method="POST">' +
+            'Student ID: <input type="Number" id="stdID" name="stdID" min="30000000" max="55000000" required><br>'+
+            'First Name: <input type="text" name="fName" required><br>'+
+            'Last Name: <input type="text" name="lName" required><br>'+
+            'Age: <input type="number" name="age" min="10" max="99" required><br>'+
+            'Gender: <input type="radio" name="gender" required>Male <input type="radio" name="gender" required>Female<br>'+
+            'Degree: <input type="text" name="degree" required><br>' +
+            '<input type="submit" name="submit" value="Submit">'+
+            '<input type="reset" name="reset" value="Reset">'+          
+        '</form>'+
+    '</body>'+
+    '</html>';
+
+response.writeHead( 200, {"Content-Type": "text/plain"} );
+response.write(body);
+response.end();
 }
 
 /**
@@ -111,5 +153,9 @@ function reqShow(response, postData) {
 
 //allow access on reqStart & reqUpload to other files
 exports.reqStart = reqStart;
+exports.reqStudent = reqStudent;
+exports.reqOption = reqOption;
+
+exports.reqView = reqView;
 exports.reqUpload = reqUpload;
 exports.reqShow = reqShow;
