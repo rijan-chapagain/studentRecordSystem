f8de289d86e2a5d90d79407ea87c789a6c9351ac
rijan <hrijon.chp@gmail.com>
2020-04-16 19:17:06 +0800

    displaying html input in json format

--- js/requestHandlers/student.js
+++ js/requestHandlers/student.js
@@ -3,6 +3,7 @@ const csv = require('csv-parser');
 var formidable = require('formidable');
 var fs = require('fs');
 const { parse } = require('querystring');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 /**
  * sends status report to server and display html for on client side
@@ -25,15 +26,55 @@ function reqStudent(request, response){
     });
 }
 
-// function reqImport(request, response){
-//     console.log("Request handler 'import' was called.");
 
-
-//     csv.
-//         write([
+/**
+ * Check is checking the incoming values using formidable
+ * 
+ * @param {object} request 
+ * @param {object} response 
+ */
+function reqImport(request, response){
+    console.log("Request handler 'import' is processing.");
+   
+    function collectRequestData(request, callback) {
+        const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+        if(request.headers['content-type'] === FORM_URLENCODED) {
             
-//         ])
+            let body = [];
 
-// }
+            response.on('error', (err) => {
+                console.error(err);
+            });
 
+            request.on('data', (chunk) => {
+                body += chunk.toString(); //convert buffer to string
+
+            });
+
+            request.on('end', () => {
+                callback(parse(body))
+
+                // response.end('ok, You all set!');
+                response.on('error', (err) => {
+                    console.error(err);
+                });
+            });
+        }
+        else{
+            callback(null);
+            console.log('callback is null...');
+        }
+    }
+
+    collectRequestData(request, result => {
+        console.log(result);
+        // data += result;
+
+        // store(request, response, result);
+
+        
+    });
+}
+
+exports.reqImport = reqImport;
 exports.reqStudent = reqStudent;
