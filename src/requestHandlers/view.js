a8f4a1766457d165a28623b3e6220b004af0cf7f
rijan <hrijon.chp@gmail.com>
2020-04-21 21:51:35 +0800

    display num of rows & setTimeOut

--- src/requestHandlers/view.js
+++ src/requestHandlers/view.js
@@ -2,6 +2,7 @@ const http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
 const { parse } = require('querystring');
+var start = require('./start');
 
 /**
  * sends status report to server and display html for on client side
@@ -24,6 +25,50 @@ function reqView(request, response){
     });
 }
 
+
+/**
+ * Check is checking the incoming values using formidable
+ * 
+ * @param {object} request 
+ * @param {object} response 
+ */
+function req(request, response){
+    console.log("Request handler 'import' is processing.");
+   
+    function collectRequestData(request, callback) {
+        const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+        if(request.headers['content-type'] === FORM_URLENCODED) {
+            
+            let body = [];
+
+            response.on('error', (err) => {
+                console.error(err);
+            });
+
+            request.on('data', (chunk) => {
+                body += chunk.toString(); //convert buffer to string
+            });
+
+            request.on('end', () => {
+                callback(parse(body));
+              
+                response.on('error', (err) => {
+                    console.error(err);
+                });
+            });
+        }
+        else{
+            callback(null);
+            console.log('callback is null...');
+        }
+    }
+
+    collectRequestData(request, result => {
+        console.log(result);
+        store(request, response, result);
+    });
+}
+
 /**
  * It will display the csv file using fs and csv
  * 
@@ -32,7 +77,7 @@ function reqView(request, response){
  */
 function reqDisplay(request, response){
     console.log("Request handler 'display' was called."); 
-
+    
     const fd = fs.openSync('../data/studentRecord.csv', 'r');
     fs.readFileSync(fd,'utf8');
 
@@ -45,10 +90,23 @@ function reqDisplay(request, response){
     var title = row[0].split(delimiter);
     var studentTableData = [];
 
-        for(var i=1; i<row.length - 1; i++){
-            studentTableData[i-1] = row[i].split(delimiter);
-        }
+    for(var i=1; i<row.length - 1; i++){
+        var data = studentTableData[i-1] = row[i].split(delimiter);
+        // console.log(data);
+        var dataArr = data[5].split(delimiter);
+    }
+    console.log("here is data Array outside for: ");
+    console.log(dataArr);
 
+    var count = 0;
+    require('fs').createReadStream("../data/studentRecord.csv")
+      .on('data', function(chunk) {
+        for (var i=0; i < chunk.length; ++i)
+          if (chunk[i] == 10) count++;
+      })
+      .on('end', function() {
+        var rowsNumber = count;
+   
     var table = "<!DOCTYPE html>" + 
                 "<html>" + 
                 "<head>" + 
@@ -60,32 +118,43 @@ function reqDisplay(request, response){
                 "<title>Students' details</title>" +
                 "<head>" + 
                 "<body>" +
-                "<h2>Details About Student </h2>"+
+                "<h2>Details About Student </h2>";
+
+                table +=`<h3>Number of rows are: ${rowsNumber}</h3>`+
                 "<table>" + 
                 "<tr>";
                
-                for (var titleIndex = 0; titleIndex < title.length; titleIndex++){
+                for(var titleIndex = 0; titleIndex < title.length; titleIndex++){
                     table += `<th>${title[titleIndex]}</th>`;
                 }
 
                 table += "</tr>";    
 
-                for (var rowIndex = 0; rowIndex < studentTableData.length; rowIndex++){
+                for(var rowIndex = 0; rowIndex < studentTableData.length; rowIndex++){
                     table += "<tr>";
                         
                     for (var colIndex = 0; colIndex < title.length; colIndex++){
-                        table += `<td>${studentTableData[rowIndex][colIndex]}</td>`;
+                        table += `<td>${studentTableData[rowIndex][colIndex]} </td>`;
                     }//end of col loop
                     table += "</tr>";
                 }//end of row loop
 
-                table += "</tr>" +              
+                table += "</tr>" + 
+                '<a href="/start"> <input type="button" class="back" name="back" value="Back to home"> </a>' +             
                 "</body>" +
                 "</html>"
-
+         
     response.writeHead( 200, {"Content-Type": "text/html"} );
     response.write(table);
+    // timeOut(request, response);
     response.end();
+
+    function timeOut(request, response){
+        setTimeout(function (){
+            start.reqStart(request, response);
+        }, 3000);
+    }  
+});
 }
 
 //allow access on reqStart & reqUpload to other files
